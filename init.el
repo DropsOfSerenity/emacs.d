9b7e603450182dce21c87757c4b7b67be8c76f01
Justin Schiff <AmbientRevolution@gmail.com>
2017-01-30 01:18:36 -0800

    aggressive indent mode

--- init.el
+++ init.el
@@ -37,14 +37,22 @@
 
 ;; Enable auto completion
 (ac-config-default)
+
+;; clean up whitespace before save
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; indent agressively
+(global-aggressive-indent-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (magit projectile haml-mode coffee-mode better-defaults auto-complete))))
+    (aggressive-indent magit projectile haml-mode coffee-mode better-defaults auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
