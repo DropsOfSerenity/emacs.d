f97f5bead8b0c242153e091afdee52ac44ce4508
Justin Schiff <AmbientRevolution@gmail.com>
2018-02-28 14:49:20 -0800

    lkajdlfkajsdlfkjasdlkf

--- init.el
+++ init.el
@@ -175,4 +175,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Fira Code")))))
+ '(default ((t (:height 140 :family "Fira Code")))))
